86401e552ad3a491e348e602a9e464785ae16b72
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-11-03 16:02:25 -0400

    Add chats button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <div className='w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100'>
+      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <div className='flex flex-col items-center text-center'>
+          <div className='flex items-center'>
+            <h1 className='mr-3 text-5xl font-semibold'>Chat with any PDF</h1>
+            <UserButton afterSignOutUrl='/' />
+          </div>
+          <div className='flex mt-2'>
+            <Button>Got to Chats</Button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
